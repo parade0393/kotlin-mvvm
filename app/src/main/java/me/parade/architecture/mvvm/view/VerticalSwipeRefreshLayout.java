f8e118ecd7086a66c5d6743b699c089d2f668acd
parade岁月 <13703937803@163.com>
2021-08-14 09:40:51 +0800

    增加自定义SwipeRefreshLayout，解决嵌套横向列表时横向列表滑动不顺畅

---
+++ app/src/main/java/me/parade/architecture/mvvm/view/VerticalSwipeRefreshLayout.java
@@ -0,0 +1,45 @@
+package me.parade.architecture.mvvm.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+/**
+ * @author : parade
+ * date : 2021/8/14
+ * description : 解决SwipeRefreshLayout与横向列表滑动冲突，增加60的容差
+ */
+public class VerticalSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    private final int mTouchSlop;
+    // 上一次触摸时的X坐标
+    private float mPrevX;
+
+    public VerticalSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // 触发移动事件的最短距离，如果小于这个距离就不触发移动控件
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPrevX = event.getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                final float eventX = event.getX();
+                float xDiff = Math.abs(eventX - mPrevX);
+                // 增加60的容差，让下拉刷新在竖直滑动时就可以触发
+                if (xDiff > mTouchSlop + 60) {
+                    return false;
+                }
+        }
+
+        return super.onInterceptTouchEvent(event);
+    }
+}
